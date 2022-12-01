30c7389574b150f88b5b32821552ae075623ef3a
JHAvanGestelDSN <jaapvg@delta-n.nl>
2022-12-01 12:14:17 +0100

    Day01

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -6,17 +6,45 @@ namespace Day01
     {
         public static void Main(string[] args)
         {
-            Program p = new();
+            Program p = new Program();
         }
 
         protected override long SolveOne()
         {
-            throw new System.NotImplementedException();
+            var list = ReadFileToArray(PathOne);
+            long max = -1;
+            long current = 0;
+            foreach (var s in list)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    max = current > max ? current : max;
+                    current = 0;
+                    continue;
+                }
+                current += long.Parse(s);
+            }
+            return max;
         }
 
         protected override long SolveTwo()
         {
-            throw new System.NotImplementedException();
+            var list = ReadFileToArray(PathOne);
+            var sortedList = new SortedSet<long>();
+            long current = 0;
+            foreach (var s in list)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    sortedList.Add(current);
+                    current = 0;
+                    continue;
+                }
+                current += long.Parse(s);
+            }
+            return sortedList.Skip(sortedList.Count - 3).Sum();
+            //todo rework to LINQ
         }
     }
+   
 }
