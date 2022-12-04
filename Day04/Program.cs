9e2ee8bd6bd1f2f8a8135d3eb0f3f45707146c87
JHAvanGestelDSN <jaapvg@delta-n.nl>
2022-12-04 15:59:59 +0100

    Day04

---
+++ Day04/Program.cs
@@ -0,0 +1,52 @@
+﻿using Day00;
+
+namespace Day04
+{
+    internal class Program : Base
+    {
+        public static void Main(string[] args)
+        {
+            Program p = new();
+        }
+
+        protected override long SolveOne()
+        {
+
+            var list = ReadFileToArray(PathOne);
+            var result = 0;
+            foreach (var s in list)
+            {
+                var elfPartnerRanges = s.Split(',');
+                var ranges = elfPartnerRanges.Select(elfPartnerRange => elfPartnerRange.Split('-')).Select(split => new Range(int.Parse(split[0]), int.Parse(split[1]))).ToList();
+                var first = ranges.First();
+                var second = ranges.Last();
+                if (first.Start.Value <= second.Start.Value && first.End.Value >= second.End.Value ||
+                    second.Start.Value <= first.Start.Value && second.End.Value >= first.End.Value)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        protected override long SolveTwo()
+        {
+            var list = ReadFileToArray(PathOne);
+            var result = 0;
+            foreach (var s in list)
+            {
+                var elfPartnerRanges = s.Split(',');
+                var ranges = elfPartnerRanges.Select(elfPartnerRange => elfPartnerRange.Split('-')).Select(split => new Range(int.Parse(split[0]), int.Parse(split[1]))).ToList();
+                var first = ranges.First();
+                var second = ranges.Last();
+                // check if the ranges overlap
+                if (first.Start.Value <= second.End.Value && second.Start.Value <= first.End.Value)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
