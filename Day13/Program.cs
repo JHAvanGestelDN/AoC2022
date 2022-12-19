11477f3f0076884d7810aaca8526f81d2ba4d522
Jaap van Gestel <jaapvg@delta-n.nl>
2022-12-19 09:55:33 +0100

    Day 13 WIP

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -13,17 +13,18 @@ namespace Day13
 
         override protected long SolveOne()
         {
-            var list = ReadFileToArray(PathOneSample);
+            var list = ReadFileToArray(PathOne).ToList();
+            list.Add("");
             string? first = null;
             string? second = null;
             int result = 0;
-            int pairIndex = 0;
+            int pairIndex = 1;
             foreach (string s in list)
             {
                 if (string.IsNullOrEmpty(s))
                 {
 
-                    var pairsInOrder = InOrder(JsonArray.Parse(first), JsonArray.Parse(second));
+                    var pairsInOrder = InOrder(JsonArray.Parse(first), JsonArray.Parse(second)); 
                     result += pairsInOrder ? pairIndex : 0;
                     first = null;
                     second = null;
@@ -41,11 +42,27 @@ namespace Day13
         public bool InOrder(JsonNode first, JsonNode second)
         {
             bool result = false;
+            if (first.AsArray().Count == 0 && second.AsArray().Count == 0)
+                return true;
+            if (first.AsArray().Count == 0 && second.AsArray().Count > 0)
+                return true;
+            if (first.AsArray().Count > 0 && second.AsArray().Count == 0)
+                return false;
 
             //deterime if both values are lists
             if (first[0].GetType().AssemblyQualifiedName.Contains("Array") && second[0].GetType().AssemblyQualifiedName.Contains("Array"))
             {
-                Console.WriteLine("lists");
+                
+                var x = JsonArray.Parse(first[0].ToJsonString());
+                var y = JsonArray.Parse(second[0].ToJsonString());
+                var compare = InOrder(x, y);
+                if (!compare)
+                    return false;
+                first.AsArray().Remove(first[0]);
+                second.AsArray().Remove(second[0]);
+                return InOrder(first, second);
+                
+
             }
 
             //determine if both values are integers
@@ -60,19 +77,25 @@ namespace Day13
                 if (firstValue == secondValue)
                 {
                     //create a new json object with evertyhing but the first value
-                    
-                    return InOrder(newFirst, newSecond);
+
+                    first.AsArray().Remove(first[0]);
+                    second.AsArray().Remove(second[0]);
+                    return InOrder(first, second);
                 }
             }
 
             // determine if one value is an integer and the other is a list
             if (first[0].GetType().AssemblyQualifiedName.Contains("Value") && second[0].GetType().AssemblyQualifiedName.Contains("Array"))
             {
-                Console.WriteLine("first int second list list");
+                var x = $"[{first[0]}]";
+                first[0] = JsonArray.Parse(x);
+                return InOrder(first, second);
             }
             if (first[0].GetType().AssemblyQualifiedName.Contains("Array") && second[0].GetType().AssemblyQualifiedName.Contains("Value"))
             {
-                Console.WriteLine("first int second list list");
+                var x = $"[{second[0]}]";
+                second[0] = JsonArray.Parse(x);
+                return InOrder(first, second);
             }
 
 
