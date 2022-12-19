a1f6307efa4367610e2113082decf04660988c50
Jaap van Gestel <jaapvg@delta-n.nl>
2022-12-19 18:49:38 +0100

    Day 14 wip

--- Day00/Coordinate.cs
+++ Day00/Coordinate.cs
@@ -27,6 +27,31 @@
             return X == other.X && Y == other.Y && Z == other.Z;
         }
 
+        public static HashSet<Coordinate> DetermineRange(Coordinate from, Coordinate to)
+        {
+            HashSet<Coordinate> coordinates = new HashSet<Coordinate>();
+
+            coordinates.Add(from);
+            coordinates.Add(to);
+            //determine x difference
+            int startIndex = from.X < to.X ? from.X : to.X;
+            int stopIndex = from.X < to.X ? to.X : from.X;
+            for (int i = startIndex; i <= stopIndex; i++)
+            {
+                coordinates.Add(new Coordinate(i, from.Y));
+            }
+            startIndex = from.Y < to.Y ? from.Y : to.Y;
+            stopIndex = from.Y < to.Y ? to.Y : from.Y;
+            for (int i = startIndex; i <= stopIndex; i++)
+            {
+                coordinates.Add(new Coordinate(from.X, i));
+            }
+
+
+
+            return coordinates;
+        }
+
         public override bool Equals(object obj)
         {
             if (ReferenceEquals(null, obj)) return false;
---
+++ Day14/Program.cs
@@ -0,0 +1,80 @@
+﻿using Day00;
+using Day00.Maps;
+using Day00.Nodes;
+
+namespace Day14
+{
+    internal class Program : Base
+    {
+        public static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        override protected long SolveOne()
+        {
+            var list = ReadFileToArray(PathOneSample);
+            HashSet<Coordinate> coordinates = new HashSet<Coordinate>();
+            ParseRocks(list, coordinates);
+            //determine the min x and y coordinates
+            int minX = 500, maxX = 500;
+            int minY = 0, maxY = 0;
+            foreach (var coordinate in coordinates)
+            {
+                if (coordinate.X < minX)
+                    minX = coordinate.X;
+                if (coordinate.X > maxX)
+                    maxX = coordinate.X;
+                if (coordinate.Y < minY)
+                    minY = coordinate.Y;
+                if (coordinate.Y > maxY)
+                    maxY = coordinate.Y;
+            }
+            CharMap map = new CharMap(maxX - minX, maxY - minY);
+            foreach (var coordinate in coordinates)
+            {
+                map.FillMap(coordinate,new CharGenericNode(coordinate,'#'));
+            }
+            map.Print();
+
+
+            throw new System.NotImplementedException();
+        }
+        private void ParseRocks(string[] list, HashSet<Coordinate> coordinates)
+        {
+
+            foreach (var line in list)
+            {
+                var split = line.Split(" -> ");
+                var first = split[0];
+                var firstCoordinate = ParseCoordinate(first);
+                coordinates.Add(firstCoordinate);
+                //iterate over split starting from index 1
+                for (int i = 1; i < split.Length; i++)
+                {
+                    //determine difference
+                    var to = ParseCoordinate(split[i]);
+                    var range = Coordinate.DetermineRange(firstCoordinate, to);
+                    foreach (var coordinate in range)
+                    {
+                        coordinates.Add(coordinate);
+                    }
+                    firstCoordinate = to;
+
+                }
+
+            }
+        }
+        public Coordinate ParseCoordinate(string input)
+        {
+            var split = input.Split(",");
+            var coordinate = new Coordinate(int.Parse(split[0]), int.Parse(split[1]));
+            return coordinate;
+        }
+
+        override protected long SolveTwo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
